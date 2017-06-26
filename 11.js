69ac13384400ecde4182b3ea0fceb5929846d85c
dvikster <dvikster@gmail.com>
2017-06-26 17:27:29 +0300

    added files

--- 11.js
+++ 11.js
@@ -3,34 +3,47 @@ var fs = require('fs');
 
 let path = "C:/xampp/htdocs/dev_upscale/D45-node.js/";
 
-
-
 fs.readdir('.', (err, files) =>{
     let counter1=0, counter2=0;
     files.forEach(file => {
-           fs.stat('.', (err, stats) => {
-                if(err)
-                    return console.log(err); //Handle error
-                if(stats.isDirectory()){
-                    ++counter2;
-                    console.log(`${file} Is directory: ${stats.isDirectory()}`);
-                }
-            });
-            fs.stat('.', (err, stats) => {
-                if(err)
-                    return console.log(err); //Handle error
-                if(stats.isFile()){
-                    ++counter1;
-                    console.log(`${file} Is file: ${stats.isFile()}`);
-                }
+        fs.stat('.', (err, stats) => {
+            if(err)
+                return console.log(err); //Handle error
+            if(stats.isDirectory()){
+                ++counter2;
+                console.log(`${file} Is directory: ${stats.isDirectory()}`);
+                console.log(counter2);
+            }
+        });
+        fs.stat('.', (err, stats) => {
+            if(err)
+                return console.log(err); //Handle error
+            if(stats.isFile()){
+                ++counter1;
+                console.log(`${file} Is file: ${stats.isFile()}`);
+            }
 
-            });
+        });
 
     });
     let counter3 = counter1 + counter2;
     console.log(counter1, counter2, counter3);
 });
 
+
+// fs.readdir('.', (err, files) =>{
+//     let counter1=0, counter2=0;
+//     files.forEach(file => {
+//         if((file.indexOf('.') == 0) && (file.indexOf('.', 1) > 0)){
+//             ++counter1;
+//         }
+//         else if((file.indexOf('.') != 0) && (file.indexOf('.', 1) > 0)) {
+//             ++counter2;
+//         }
+//     });
+//     let counter3 = counter1 + counter2;
+//     console.log(counter1, counter2, counter3);
+// });
 
 
 // fs.readdir('.', (err, dirs) => {
